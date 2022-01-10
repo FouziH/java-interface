451193bf3f115f10fd3993556dabbf139cd5b676
Hamza Mohamed <Hamzafowzi1@gmail.com>
2022-01-10 08:29:16 -0600

    ...

--- Inheritance.java
+++ Inheritance.java
@@ -2,6 +2,14 @@ public class Inheritance {
     public static void main (String[] args) {
         Dog dog = new Dog();
         dog.makeSound();
+
+        Bird bird = new Bird();
+        bird.makeSound();
+
+        Person firstPerson = new Person();
+        firstPerson.printFullName();
+
+        Object[] list = {new Dog(), new DogFood()}
     }
 }
  class Animal {
@@ -16,5 +24,32 @@ class Dog extends Animal {
     Dog() {
         sound ="bark";
     }
+
+}
+
+class Bird extends Animal {
+    Bird () {
+        sound ="Quack Quack";
+    }
+}
+
+class Human {
+    String firstName = "";
+    String lastName = "";
+
+    void printFullName () {
+        System.out.println(firstName + " " + lastName);
+    }
+
+}
+
+class Person extends  Human {
+   Person () {
+       firstName = "Hamza";
+       lastName = "Mohamed";
+    }
+}
+
+class DogFood {
 
 }
