1a316ea62ee5be1e4976dcd3f4933f06dbfbe7c9
Hamza Mohamed <Hamzafowzi1@gmail.com>
2022-01-10 08:01:29 -0600

    ...

--- Inheritance.java
+++ Inheritance.java
@@ -1,6 +1,7 @@
 public class Inheritance {
     public static void main (String[] args) {
-        //write your code here 
+        Dog dog = new Dog();
+        dog.makeSound();
     }
 }
  class Animal {
@@ -8,5 +9,9 @@ public class Inheritance {
      void makeSound () {
          System.out.println(sound);
      }
+
+}
+
+class Dog extends Animal {
 
 }
