ae515860d0ce51f957d0f85aa6124a5b722e08cb
Hamza Mohamed <Hamzafowzi1@gmail.com>
2022-01-13 08:35:15 -0600

    ...

---
+++ Store.java
@@ -0,0 +1,15 @@
+public class Store {
+    String productType;
+
+    public Store(String product) {
+        productType = product;
+    }
+    
+    public static void main(String[] args) {
+
+        Store lemonadeStand = new Store("lemonade");
+
+        System.out.println(lemonadeStand.productType);
+
+    }
+}
